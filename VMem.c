da610d40845f3f1429daf4d5416f96bd861400d6
jayletato <jayjtrainer@gmail.com>
2024-11-23 05:07:46 +0000

    testing in VMem.c

--- VMem.c
+++ VMem.c
@@ -1,13 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+
+#define PAGE_SIZE 4096 // 4 KiB of memory 
 
 void main() {
     int * ptr;
-    ptr = (int *) malloc(100 * sizeof(int));
+    ptr = malloc(PAGE_SIZE);
     if (ptr = NULL) {
         printf("Memory allocation unsuccessful!\n");
         exit(EXIT_FAILURE);
     }
 
+    printf("The virtual memory address starts at : %p\n", &ptr);
     free(ptr);
 }
