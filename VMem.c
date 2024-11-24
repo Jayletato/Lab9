ffdfd645ce8b0edb8f239f8a9925ff806e60549a
jayletato <jayjtrainer@gmail.com>
2024-11-24 08:03:33 +0000

    printsaddress

--- VMem.c
+++ VMem.c
@@ -2,16 +2,31 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define PAGE_SIZE 4096 // 4 KiB of memory 
+#define PAGE_SIZE 4096 // 4 KiB of memory
+#define OFFSET_SIZE 512 //  
 
-void main() {
-    int * ptr;
+int main() {
+    void  *ptr;
     ptr = malloc(PAGE_SIZE);
     if (ptr = NULL) {
         printf("Memory allocation unsuccessful!\n");
         exit(EXIT_FAILURE);
     }
-
     printf("The virtual memory address starts at : %p\n", &ptr);
+
+    long int virtualAddress = (long int) &ptr;
+
+    printf("virtual_address = %#lx\n", virtualAddress);
+
+    long int pageOffset = 
+
+    
+    
+    
+    
+    
+    
     free(ptr);
+
+    exit(EXIT_SUCCESS);
 }
